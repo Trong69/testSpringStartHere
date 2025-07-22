68e7a00996d25696b3ef4867a72af6c4c7fb0b0d
APAC\GNR9HC <Trung.NguyenThe@vn.bosch.com>
2025-07-22 17:14:37 +0700

    main 5

--- src/main/java/com/example/service/CommentService.java
+++ src/main/java/com/example/service/CommentService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 public class CommentService {
 
   public CommentService(){
-    System.out.println("main 4");
+    System.out.println("main 5");
 
   }
 
