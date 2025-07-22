ee565ee2dc9f486ffe66fa973756597220ab1ef9
APAC\GNR9HC <Trung.NguyenThe@vn.bosch.com>
2025-07-22 17:13:33 +0700

    feature 5

--- src/main/java/com/example/service/CommentService.java
+++ src/main/java/com/example/service/CommentService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 public class CommentService {
 
   public CommentService(){
+    System.out.println("feature 5");
     System.out.println("feature 4");
     System.out.println("main 5");
 
